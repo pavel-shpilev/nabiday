29e91a830a87b3673bf6c6cb6ed50ee8ca2eb5b4
Pavel Shpilev <p.shpilev@gmail.com>
2014-12-06 16:21:49 +1100

    Mapper

--- nab_iday/views.py
+++ nab_iday/views.py
@@ -24,14 +24,33 @@ def accounts(request):
     return HttpResponse(html)
 
 
+def magic_mapper(desc):
+    return {
+        'Mahadev': 'McDonalds',
+        'LinkedAccount': 'Pharmacy',
+        'Now': 'Bike Shop',
+        '$10 6-Jan USav-Ult': 'Coles',
+        'Another test': 'Gym',
+        'Test': 'Subway',
+        '2c now test': '',
+        'Now 1c test': '',
+        '$20 now Usav-Ultra': 'Vegie Shop',
+        '$15 now Usav-Ultra': '',
+        '$25 now Usav-Ultra': '',
+        '$.01 Usaver-Ultra': '',
+        '$3 Usaver to Ultra': '',
+        'Test nabdev.com.au': '',
+    }.get(desc, desc)
+
 def transactions(request, acc_token):
     api = NabApi()
     auth_token = api.login()['tokens'][0]['value']
 
     html = "<html><body>Transactions:<ul>"
     for transaction in api.transactions(auth_token, acc_token)['transactionsResponse']['transactions']:
-        html += '<li>{0} {1} {2} {3}</li>'.format(
-            transaction['date'], transaction['description'], transaction['narrative'], transaction['amount'])
+        html += '<li>{0} {1} {2}</li>'.format(
+            transaction['date'], magic_mapper(transaction['description']),
+            transaction['amount'])
     html += "</ul></body></html>"
 
     return HttpResponse(html)
