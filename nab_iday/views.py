e36f99a25971d98fa41baa74050b9e0f98311fc2
Pavel Shpilev <p.shpilev@gmail.com>
2014-12-07 13:18:04 +1100

    simulated API. Charts on the right.

--- nab_iday/views.py
+++ nab_iday/views.py
@@ -56,18 +56,37 @@ def transactions(request, acc_token):
     # Not exactly sure why this is needed in this view but whatevs (breaks
     # if you take it out).
     api = NabApi()
-    auth_token = api.login()['tokens'][0]['value']
+#    auth_token = api.login()['tokens'][0]['value']
 
     return render(request, "transactions.html", {}, context_instance=RequestContext(request))
 
 
 def transactions_json(request, acc_token):
     api = NabApi()
-    auth_token = api.login()['tokens'][0]['value']
+#    auth_token = api.login()['tokens'][0]['value']
 
-    ts = api.transactions(auth_token, acc_token)['transactionsResponse']['transactions']
+#    ts = api.transactions(auth_token, acc_token)['transactionsResponse']['transactions']
+    ts = [
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'asdf', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blasdfaah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blaasdfh', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'asdf', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blasdfsh', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'asdf', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blasdfaah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blasdfaah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'asdf', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blah', 'date': '2001-12-06', 'amount': '10'},
+        {'description': 'blasdfaah', 'date': '2001-12-06', 'amount': '10'},
+    ]
 
     transactions = []
+
     places = set([])
 
     running_total = []
